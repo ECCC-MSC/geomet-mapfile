cda7caa28a10b94a7df6e219238a4a364262ec2d
etiennepelletier <etienne.pelletier@canada.ca>
2019-10-11 08:26:27 -0400

    added style conversion utility

--- geomet3_mapfile/utils.py
+++ geomet3_mapfile/utils.py
@@ -10,8 +10,12 @@ import mappyfile
 def convert_style_to_json(file):
     with open(file, 'r') as f:
         file = f.readlines()
-        print(file)
-
+        start_postition = None
+        for index, line in enumerate(file):
+            if "CLASS\n" in line:
+                start_postition = index
+                break
+        return json.dumps(mappyfile.loads("".join(file[start_postition:])), indent=4)
 @click.group()
 def utils():
     pass
